827490709dd93e2eda35df78a4fe88ca68aaee28
Your Name <you@example.com>
2024-04-04 19:29:11 +0200

    add setup_oracle.sh

--- setup_oracle.sh
+++ setup_oracle.sh
@@ -2,10 +2,14 @@
 
 spawn /etc/init.d/oracle-xe-18c configure
 
-expect "Specify a password to be used for database accounts. Oracle recommends that the password entered should be at least 8 characters in length, contain at least 1 uppercase character, 1 lower case character and 1 digit [0-9]. Note that the same password will be used for SYS, SYSTEM and PDBADMIN accounts:oracle"
+expect "Specify a password to be used for database accounts. Oracle recommends that the password entered should be at least 8 characters in length, contain at least 1 uppercase character, 1 lower case character and 1 digit \\\[0-9\\\]. Note that the same password will be used for SYS, SYSTEM and PDBADMIN accounts:\r"
+#expect -re {Specify a password to be used for database $}
 send "oracle\r"
 
-expect "Confirm the password:"
+expect "Confirm the password:\r"
+#expect -re {Confirm the password $}
 send "oracle\r"
+
+#expect -re {Specify a password to be used}
 
 expect eof
