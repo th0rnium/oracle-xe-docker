875894ddc9d8dfa4189e291883d2a4649059d87c
Your Name <you@example.com>
2024-04-08 08:45:47 +0200

    .

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #docker run --rm -it -p 1521:1521 -p 5500:5500 -h devXE --name devXE oracle-db:18cXE
-docker run -p 1521:1521 -p 5500:5500 --rm -it  -h devXE --name devXE oracle-db:18cXE
+docker run --rm -p 1521:1521 -p 5500:5500 --rm -it  -h devXE --name devXE oracle-db:18cXE
 #docker run -it -p 1521:1521 -p 5500:5500 -h devXE --name devXE oracle-db:18cXE
