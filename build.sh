3d64b8b9679f89ca4aad61858b3afb4566d2ec9c
Your Name <you@example.com>
2024-04-08 08:15:17 +0200

    .

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #docker build --no-cache --progress plain -f dockerfiles/Dockerfile -t oracle-db:18cXE .
-docker build --interactive--build-arg CACHEBUST=$(date +%s) --progress plain -f dockerfiles/Dockerfile -t oracle-db:18cXE .
+docker build --build-arg CACHEBUST=$(date +%s) --progress plain -f dockerfiles/Dockerfile -t oracle-db:18cXE .
