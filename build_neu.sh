49e2207db760031514a51f8b7d854086a0b95a73
Your Name <you@example.com>
2024-04-11 08:01:10 +0200

    rm unused files.

--- build_neu.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#docker build --no-cache --progress plain -f dockerfiles/Dockerfile -t oracle-db:18cXE .
-docker build --build-arg CACHEBUST=$(date +%s) --progress plain -f dockerfiles/Dockerfile -t oracle-db:18cXE_neu .
--- run_neu.sh
+++
@@ -1,1 +0,0 @@
-docker run -p 1522:1521 -p 5500:5500 -it  -h devXE --name devXE_neu oracle-db:18cXE_neu
